a4194e7b836e44e2542b6dca3f39d10c4069de68
arguilla <arguilla@student.42.fr>
2021-02-27 17:15:42 +0100

    mise en place du certificat ssl en cours

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apt-get -yqq update && apt-get install -qq -y vim
 
 RUN apt-get -yqq update && apt-get install -qq -y curl vim wget
 RUN apt-get update && apt-get install -y -qq nginx
-#RUN apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
+RUN apt-get -y install php7.3-{fpm,common,mysql,gmp,curl,intl,mbstring,xmlrpc,gd,xml,cli,zip,soap,imap}
 #RUN apt-get -y install mariadb-server
 # install ssl 
 #RUN apt-get update && apt-get install -qq -y openssl
--- src/run.sh
+++ src/run.sh
@@ -12,17 +12,25 @@ display_msg () {
 # ssl
 display_msg $GREEN "Generation du certificat SSL."
 mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42Paris/OU=arguilla/CN=localhost"
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/site.pem -keyout /etc/nginx/ssl/site.key -subj "/C=FR/ST=Paris/L=Paris/O=42Paris/OU=arguilla/CN=site"
 
-#nginx
+# website
+mkdir /var/www/site && touch /var/www/site/index.php
+echo "<h1>Bienvenue !</h1><?= phpinfo(); ?>" >> /var/www/site/index.php
+
+# nginx
 display_msg $GREEN "Mise en place du fichier de configuration nginx."
-mv nginx-default /etc/nginx/sites-available
-mv /etc/nginx/sites-available/nginx-default /etc/nginx/sites-available/default
+mv nginx-default /etc/nginx/sites-available/site
+ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled/site
+rm -rf /etc/nginx/sites-enabled/default
+display_msg $GREEN "Permissions du serveur pour site."
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
 
 # service start
 display_msg $GREEN "Lancement du serveur nginx."
 service nginx start
 
 # start the shell
-display_msg ${GREEN} "Welcome in my container!"
+display_msg ${GREEN} "Bienvenue!"
 bash
