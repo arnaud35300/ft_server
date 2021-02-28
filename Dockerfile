78ab15b72065cf4f56b8ed0f792e6817f6c16015
arguilla <arguilla@student.42.fr>
2021-02-28 08:23:37 +0100

    installation de php et configuration de la bdd en cours

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,20 @@ FROM debian:buster
 # author
 MAINTAINER arguilla <arguilla@student.42.fr>
 
-# install vim
-RUN apt-get -yqq update && apt-get install -qq -y vim
+# install utils
+RUN apt-get -yqq update && apt-get install -yqq curl vim wget
+
+# install ssl 
+RUN apt-get update && apt-get install -qq -y openssl
 
 # install nginx
+RUN apt-get -yqq update && apt-get install -yqq nginx
 
-RUN apt-get -yqq update && apt-get install -qq -y curl vim wget
-RUN apt-get update && apt-get install -y -qq nginx
-RUN apt-get -y install php7.3-{fpm,common,mysql,gmp,curl,intl,mbstring,xmlrpc,gd,xml,cli,zip,soap,imap}
-#RUN apt-get -y install mariadb-server
-# install ssl 
-#RUN apt-get update && apt-get install -qq -y openssl
+# install PHP
+RUN apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
+
+# install mariadb
+RUN apt-get -y install mariadb-server
 
 #copy run script in the container
 COPY src/run.sh .
--- src/run.sh
+++ src/run.sh
@@ -12,11 +12,11 @@ display_msg () {
 # ssl
 display_msg $GREEN "Generation du certificat SSL."
 mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/site.pem -keyout /etc/nginx/ssl/site.key -subj "/C=FR/ST=Paris/L=Paris/O=42Paris/OU=arguilla/CN=site"
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/site.pem -keyout /etc/nginx/ssl/site.key -subj "/C=FR/ST=Paris/L=Paris/O=42Paris/OU=arguilla/CN=site" &> /dev/null
 
 # website
 mkdir /var/www/site && touch /var/www/site/index.php
-echo "<h1>Bienvenue !</h1><?= phpinfo(); ?>" >> /var/www/site/index.php
+echo "<h1>Bienvenue !</h1><p>Si un nombre aleatoire s'affiche, c'est que PHP fonctionne.</p><h2><?= rand(0,100); ?></h2>" >> /var/www/site/index.php
 
 # nginx
 display_msg $GREEN "Mise en place du fichier de configuration nginx."
@@ -27,9 +27,17 @@ display_msg $GREEN "Permissions du serveur pour site."
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
+# mysql
+service mysql start
+echo "CREATE DATABASE wordpress" | mysql -u root --skip-password
+
 # service start
 display_msg $GREEN "Lancement du serveur nginx."
 service nginx start
+
+# service php-fpm
+display_msg $GREEN "Lancement de PHP."
+service php7.3-fpm start
 
 # start the shell
 display_msg ${GREEN} "Bienvenue!"
